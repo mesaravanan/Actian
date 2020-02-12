056a15cbddcdcc7a73033985537189b78f3c370f
mesaravanan <saravan.m89@gmail.com>
2020-02-12 11:07:09 +0530

    Ngx Cacheable module added

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,23 +1,17 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../environments/environment";
-import { Observable, of } from "rxjs";
+import { Cacheable } from "ngx-cacheable";
 
 @Injectable({
   providedIn: "root"
 })
 export class ApiService {
-  public responseCache = new Map();
   constructor(private http: HttpClient) {}
-
-  getCityLocation(city: string): Observable<any> {
+  @Cacheable()
+  getCityLocation(city: string) {
     const url = environment.apis.geoCode + city;
-    const locationFromCache = this.responseCache.get(url);
-    if (locationFromCache) {
-      return of(locationFromCache);
-    }
     const response = this.http.get<any>(url);
-    response.subscribe(res => this.responseCache.set(url, res));
     return response;
   }
 }
